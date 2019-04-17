b1b15ea6d8edfda3c3210be625c1d0e234ad962b
Ry Fleming <ry.fleming@ou.edu>
2019-04-16 22:17:44 -0500

    arrange shapes

--- src/DrawFrame.java
+++ src/DrawFrame.java
@@ -16,26 +16,39 @@ public class DrawFrame extends JFrame {
 		
 		Point pointUL = new Point(300,300);
 		Point pointUR = new Point(500, 500);
-		Square square = new Square(pointUL, 100, Color.BLUE, true);
-		Oval oval = new Oval(pointUL, 100, 200, Color.BLUE, true);
-		Circle circle = new Circle(pointUL, 100, Color.BLUE, true);
-		PolyLine polyLine = new PolyLine(pointUL, pointUR, 30, Color.BLACK, true);
-		RightTriangle rightTriangle = new RightTriangle(pointUL, 100, 200, Color.MAGENTA, true);
-		Rectangle rectangle = new Rectangle(pointUL, 200, 100, Color.DARK_GRAY, true);
-		IsoscelesTriangle isoTriangle = new IsoscelesTriangle(pointUL, 200, -200, Color.RED, true);
-		Diamond diamond = new Diamond(pointUL, 200, Color.YELLOW, true);
 		
+		Point sqr = new Point(380, 275);
+		Square square = new Square(sqr, 100, Color.WHITE, true);
 		
+		Point ova = new Point(260,250);
+		Oval oval = new Oval(ova, 100, 200, Color.BLUE, true);
 		
-		//drawPanel.addShape(circle);
-		//drawPanel.addShape(oval);
-		//drawPanel.addShape(square);
-		//drawPanel.addShape(polyLine);
-		//drawPanel.addShape(rightTriangle);
-		//drawPanel.addShape(rectangle);
-		//drawPanel.addShape(isoTriangle);
+		Point circ = new Point(300,150);
+		Circle circle = new Circle(circ, 100, Color.ORANGE, true);
+		
+		Point poly = new Point(125, 125);
+		PolyLine polyLine = new PolyLine(poly, pointUL, 15, Color.BLACK, true);
+		
+		Point rTri = new Point(80, 300);
+		RightTriangle rightTriangle = new RightTriangle(rTri, 100, -200, Color.MAGENTA, true);
+		
+		Point rect = new Point(300, 200);
+		Rectangle rectangle = new Rectangle(rect, 200, 100, Color.DARK_GRAY, true);
+		
+		Point iso = new Point(150, 300);
+		IsoscelesTriangle isoTriangle = new IsoscelesTriangle(iso, 200, -200, Color.RED, true);
+		
+		Point diam = new Point(18, 300);
+		Diamond diamond = new Diamond(diam, 200, Color.YELLOW, true);
+		
+		drawPanel.addShape(rectangle);
+		drawPanel.addShape(square);
+		drawPanel.addShape(isoTriangle);
 		drawPanel.addShape(diamond);
-		
+		drawPanel.addShape(polyLine);
+		drawPanel.addShape(rightTriangle);
+		drawPanel.addShape(circle);
+		drawPanel.addShape(oval);
 		
 		add(drawPanel);
 		
