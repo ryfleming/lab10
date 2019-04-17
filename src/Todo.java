1533827b72141bd973f1d49287d57957c4c558ea
Ry Fleming <ry.fleming@ou.edu>
2019-04-16 23:09:39 -0500

    upload TODO

---
+++ src/Todo.java
@@ -0,0 +1,33 @@
+public class Todo {/*
+Lab Section_________: 12
+Last Modified_______: 2019-04-16
+OU ID_______________: 112663299
+OU EMAIL____________: ry.j.fleming-1@ou.edu
+
+Tasks:
+1. Implement Classes
+2. Test Classes
+3. Fix Bugs
+4. Draw Image
+5. Save Image to File
+6. Embed Image in README
+7. Generate JUnit Report
+
+Estimated Time (minutes):
+1. 480
+2. 200
+3. 60
+4. 60
+5. 20
+6. 10
+7. 10
+
+Actual Time (minutes):
+1. 540
+2. 30
+3. 120
+4. 45
+5. 10
+6. 10
+7. 10
+*/}
